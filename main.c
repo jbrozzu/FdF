ad719d218e317472e50f5ebc7fc427e06692ac37
Jeremie BROZZU <jbrozzu@e2r11p23.42.fr>
2015-01-21 17:26:59 +0100

    new

---
+++ main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbrozzu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/12 15:18:04 by jbrozzu           #+#    #+#             */
+/*   Updated: 2015/01/21 16:15:40 by jbrozzu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+#include "libft/libft.h"
+
+int		key_hook(int keycode)
+{
+	if (keycode == 65307)
+		exit(0);
+	return (0);
+}
+
+int		expose_hook(t_env *e)
+{
+	int fd;
+
+	fd = open(e->argv, O_RDONLY);
+	if (fd == -1)
+	{
+		perror(e->argv);
+		exit(0);
+	}
+	reader(fd, e);
+	return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	t_env e;
+	
+	if (argc == 2)
+	{
+		e.mlx = mlx_init();
+		e.win = mlx_new_window(e.mlx, 1500, 900, "42");
+		e.argv = ft_strdup(argv[1]);
+		mlx_key_hook(e.win, key_hook, &e);
+		mlx_expose_hook(e.win, expose_hook, &e);
+		mlx_loop(e.mlx);
+	}
+	else
+		write(1, "usage : ./fdf file_name\n", 24);
+	return (0);
+}
